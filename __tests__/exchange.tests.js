68b41fea4257d9d9bb01e8c38b6084a28eba9261
Kai Kohlsdorf <kai.w.kohlsdorf@gmail.com>
2022-07-04 20:48:34 -0700

    Add conversion options, attempt to fix errors, work on code, WIP.

---
+++ __tests__/exchange.tests.js
@@ -0,0 +1,14 @@
+import { exportAllDeclaration } from '@babel/types';
+import test from 'node:test';
+import ExchangeRate from './../src/exchange.js';
+
+describe('ExchangeRate', () => {
+
+  test('should correctly return the currency amount input by the user in exchanged rate', () =>
+  {
+    const response = new ExchangeRate(begin);
+    expect(currency).toEqual()
+
+  }
+  )
+})
--- src/js/exchange.js
+++ src/js/exchange.js
@@ -1,14 +1,26 @@
-//business logic
-
-class Template {
-
-  constructor() {
-    this.stuff = []
+export default class ExchangeRate {  
+  
+  static async getExchangeRate(currencyFrom, currencyTo, currencyAmt) {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currencyFrom}/${currencyTo}/${currencyAmt}}`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch(error) {
+      return error.message;
+    }
   }
-
-  addJunk(array) {
-    array.forEach((thing) => {
-      this.stuff.push(thing);
-    });
+  
+  static async getList() {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch(error) {
+      return error.message;
+    }
   }
 }
