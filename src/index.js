bb8e4c542329bb2248875452a3340fba10fd8f39
Kai Kohlsdorf <kai.w.kohlsdorf@gmail.com>
2022-07-17 15:43:43 -0700

    Fix errors until npm run start works.

--- src/index.js
+++ src/index.js
@@ -2,81 +2,25 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import { ExchangeRate, convertMonies } from './js/exchange.js';
+import { ExchangeRate } from './js/exchange.js';
 
-function getElements(response) {
-  if (response.main) {
-    $('.showRate').text('The exchange rate is ${response.main.}.');
-    $('.showAmt').text('The currency amount is ${response.main.}.');
-  } else {
-    $('showErrors').text('There was an error: ${response}');
-  }
-}
-
-async function makeApiCall1(currencyFrom, currencyTo, currencyAmt) {
-  const response = await ExchangeRate.getExchangeRate(currencyFrom, currencyTo, currencyAmt);
-  getElements(response);
-}
-
-async function makeApiCall2() {
-  const response = await ExchangeRate.getList();
-  getElements(response);
-}
+// async function makeApiCall1(currencyFrom, currencyTo, currencyAmt) {
+//   const response = await ExchangeRate.getExchangeRate(currencyFrom, currencyTo, currencyAmt);
+// }
 
 $(document).ready(function() {
   $('#exchange').click(function() {
     let currencyTo = $('#currency-to').val();
     let currencyFrom = $('#currency-from').val();
     let currencyAmt = $('#amount').val();
-    makeApiCall1(currencyFrom, currencyTo, currencyAmt);
-    makeApiCall2();
-  })
-  
-  function loadList() {
-    const conversionOptions = {
-      'AED': 'UAE Dirham - United Arab Emirates', 
-      'AFN': 'Afghan Afghani - Afghanistan',
-      'ALL': 'Albanian Lek - Albania', 
-      'AMD': 'Armenian Dram - Armenia',	
-      'ANG': 'Netherlands Antillian Guilder - Netherlands Antilles',
-      'AOA':	'Angolan Kwanza - Angola',
-      'ARS': 'Argentine Peso - Argentina', 
-      'AUD': 'Australian Dollar - Australia', 
-      'AWG': 'Aruban Florin - Aruba', 
-      'AZN': 'Azerbaijani Manat - Azerbaijan', 
-      'BAM': 'Bosnia and Herzegovina Mark - Bosnia and Herzegovina', 
-      'BBD': 'Barbados Dollar - Barbados', 
-      'BDT': 'Bangladeshi Taka - Bangladesh', 
-      'BGN': 'Bulgarian Lev -	Bulgaria', 
-      'BHD': 'Bahraini Dinar - Bahrain', 
-      'BIF': 'Burundian Franc	- Burundi'
-    };
-
-    for (let list in currency) {
-
-      $("#currencyFrom").append(`<option>${list}</option>`);
-      $("#currencyTo").append(`<option>${list}</option>`);
-    }
-  }
-
-  function displayResults(currencyTo) {
-  try {
-    if (result) {
-     $("#showAmt").html(currencyTo);
-
-     $("#error").hide();
-     $("#results").slideDown();
+    // makeApiCall1(currencyFrom, currencyTo, currencyAmt);
+    if (currencyAmt > 0) {
+      ExchangeRate.getExchangeRate(currencyFrom, currencyTo, currencyAmt)
+        .then(function() {
+          $("#showAmt").html(currencyTo);
+        });
     } else {
-      throw new Error("The currency requested is not available.");
+      $('.showErrors').text("You must input a positive number.");
     }
-  } catch (error) {
-    displayError(error);
-  }
-}
-
-  function displayError(error) {
-    document.getElementsById("error");
-    $("#results").hide();
-    $("#error").slideDown();
-  }
-})
+  });
+});
--- src/js/exchange.js
+++ src/js/exchange.js
@@ -1,38 +1,15 @@
 export class ExchangeRate {  
   static async getExchangeRate(currencyFrom, currencyTo, currencyAmt) {
-    try {
-      let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currencyFrom}/${currencyTo}/${currencyAmt}`);
-      if (!response.ok) {
+    let url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currencyFrom}/${currencyTo}/${currencyAmt}`;
+    return fetch(url)
+    .then(function(response) {
+      if(!response.ok) {
         throw Error(response.statusText);
-      }
+      }       
       return response.json();
-    } catch(error) {
-      return error.message;
-    }
-  }
-  
-  static async getList() {
-    try {
-      let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/codes`);
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      return response.json();
-    } catch(error) {
-      return error.message;
-    }
-  }
-}
-
-export function convertMonies(currencyAmt, currencyTo, response) {
-  if (response) {
-    let result;
-
-    for (let list in response) {
-      if (list === currencyTo) {
-        result = (currencyAmt * response[list]).toFixed(2);
-      }
-    }
-    return result;
+    })
+    .catch(function(error) {
+      return error;
+    });
   }
 }
